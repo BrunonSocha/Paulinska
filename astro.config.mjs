cb07161494adbab1a836d2f9214c4283c14206ac
BrunonSocha <brunon.socha@gmail.com>
2025-10-28 17:17:47 +0100

    deployment attempt no 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-const REPO_NAME = 'Paulinska'
-
 // https://astro.build/config
 export default defineConfig({
-    base: '/{$REPO_NAME}/',
-    site: 'https://brunonsocha.github.io/${REPO_NAME}',
+    base: 'Paulinska',
+    site: 'https://brunonsocha.github.io/Paulinska',
     vite: {
         plugins: [tailwindcss()],
     },
